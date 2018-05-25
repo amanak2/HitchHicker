53e66377049dc1e0bfccc1583d83620e9b0cea8a
Aman Chawla <aman.chawla96@gmail.com>
2018-05-25 15:48:25 +0530

    Animating Launch Screen

--- HitchHicker/Controller/HomeVC.swift
+++ HitchHicker/Controller/HomeVC.swift
@@ -8,18 +8,26 @@
 
 import UIKit
 import MapKit
+import RevealingSplashView
 
 class HomeVC: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
-    
     @IBOutlet weak var actionBtn: RoundedShadowBtn!
     
     var delegate: CenterVCDelegate?
     
+    var revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "launchScreenIcon")!, iconInitialSize: CGSize(width: 80, height: 80), backgroundColor: UIColor.white)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        
+        self.view.addSubview(revealingSplashView)
+        revealingSplashView.animationType = SplashAnimationType.heartBeat
+        revealingSplashView.startAnimation()
+        
+        revealingSplashView.heartAttack = true 
     }
 
     @IBAction func actionBtnPressed(_ sender: Any) {
