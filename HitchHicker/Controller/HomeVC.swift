581d37f4892fafeae597c8a18c4fc5da9e921037
Aman Chawla <aman.chawla96@gmail.com>
2018-05-27 13:58:11 +0530

    User Location Tracking

--- HitchHicker/Controller/HomeVC.swift
+++ HitchHicker/Controller/HomeVC.swift
@@ -11,7 +11,7 @@ import MapKit
 import CoreLocation
 import RevealingSplashView
 
-class HomeVC: UIViewController, MKMapViewDelegate {
+class HomeVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var actionBtn: RoundedShadowBtn!
@@ -75,6 +75,14 @@ extension HomeVC: CLLocationManagerDelegate {
             mapView.showsUserLocation = true
             mapView.userTrackingMode = .follow
         }
+    }
+}
+
+extension HomeVC: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        UpdateService.instance.updateUserLocation(withCoordinate: userLocation.coordinate)
+        UpdateService.instance.updateDriverLocation(withCoordinate: userLocation.coordinate)
     }
 }
 
